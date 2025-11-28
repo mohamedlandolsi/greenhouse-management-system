1396001673a9a27424ea655551215694fc82bc33
Mohamed Landolsi <mohamedlandolsi300@gmail.com>
2025-11-28 10:38:43 +0100

    fix(java): Replace javax.annotation with jakarta.annotation for Java 17
    - Changed PostConstruct import from javax.annotation to jakarta.annotation
    - Required for Java 17+ / Jakarta EE 9+ compatibility

--- service-discovery/src/main/java/com/greenhouse/discovery/config/EurekaServerConfig.java
+++ service-discovery/src/main/java/com/greenhouse/discovery/config/EurekaServerConfig.java
@@ -6,7 +6,7 @@ import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class EurekaServerConfig {
